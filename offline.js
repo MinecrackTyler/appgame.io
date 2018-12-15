124517fdab47069f511111b7a55cde8c9ca57da1
MinecrackTyler <45902457+MinecrackTyler@users.noreply.github.com>
2018-12-15 12:56:25 -0600

    Add files via upload

---
+++ offline.js
@@ -0,0 +1,25 @@
+﻿{
+	"version": 1529700754,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"images/obj_player-sheet0.png",
+		"images/tiledbackground.png",
+		"images/obj_food-sheet0.png",
+		"images/obj_projectile-sheet0.png",
+		"images/obj_block-sheet0.png",
+		"images/obj_adversary-sheet0.png",
+		"images/particles.png",
+		"media/nyangabe.m4a",
+		"media/nyangabe.ogg",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png",
+		"Photon-Javascript_SDK.min.js"
+	]
+}
